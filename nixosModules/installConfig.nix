dedd156283289411d581e3c93505093941c277b3
Qubic <ThatQubicFox@protonmail.com>
2026-07-22 00:40:12 +0200

    fix bad option name

--- nixosModules/installConfig.nix
+++ nixosModules/installConfig.nix
@@ -20,7 +20,7 @@
       };
     }
   '';
-  defaultConfigTemplate = ''
+  configuration = ''
     # Edit this configuration file to define what should be installed on
     # your system. Help is available in the configuration.nix(5) man page, on
     # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
@@ -136,7 +136,7 @@
 in {
 
   system.nixos-generate-config = {
-    inherit flake defaultConfigTemplate;
+    inherit flake configuration;
   };
 
   # Set it to use flake
